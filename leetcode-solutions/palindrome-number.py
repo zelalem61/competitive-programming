bff02711bb2d5e5906408fbb25b7d0a7fecc5398
zelalem61 <108578495+zelalem61@users.noreply.github.com>
2023-11-29 18:03:24 +0300

    Added solution for Palindrome Number

--- leetcode-solutions/palindrome-number.py
+++ leetcode-solutions/palindrome-number.py
@@ -1,21 +1,8 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        if x < 0:
-            return False
-        elif x < 10:
+        
+        strin = str(x)
+        if strin == strin[::-1]:
             return True
-        elif x < 100:
-            if x //10 == x % 10:
-                return True
-            else:
-                return False
-        else:
-            num = str(x)
-            for i in range(len(num)):
-                if num[i]==num[-1*(i+1)]:
-                    truth= True
-                else: 
-                    truth= False
-                    break
-            return truth        
+        return False
         
