1dd1d00cca972091b1ed7d7be09e8d2a549c5ec2
Zelalem Habtamu <108578495+zelalem61@users.noreply.github.com>
2023-12-11 08:38:54 -0800

    Added solution for Card Flipping Game

---
+++ leetcode-solutions/card-flipping-game.py
@@ -0,0 +1,10 @@
+class Solution:
+    def flipgame(self, fronts: List[int], backs: List[int]) -> int:
+        s = set(fronts+backs)
+        
+        for i in range(len(fronts)):
+            if fronts[i] == backs[i] and fronts[i] in s:
+                s.remove(fronts[i])
+        if len(s) == 0:
+            return 0
+        return s.pop()
