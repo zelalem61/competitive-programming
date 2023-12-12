cbae4581593cbf5a2e0d7a3b928f9c1a99451ce0
Zelalem Habtamu <108578495+zelalem61@users.noreply.github.com>
2023-12-12 03:27:42 -0800

    Added solution for Happy Number

---
+++ leetcode-solutions/happy-number.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def isHappy(self, n):
+       
+        visitedNumbers = set()
+        while n not in visitedNumbers:
+            visitedNumbers.add(n)
+            n = self.squaring(n)
+            if n == 1:
+                return True
+            
+        return False
+    
+    def squaring(self,n):
+        squareCount = 0
+        while n > 0:
+            numberMod = n % 10
+            numberMod = numberMod * numberMod
+            squareCount += numberMod
+            n = n // 10
+        return squareCount 
