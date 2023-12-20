139bffc22e02e56d73e970c1ccc15da16fda35b4
Zelalem Habtamu <108578495+zelalem61@users.noreply.github.com>
2023-12-20 04:09:20 -0800

    Update reverse-string.py

--- leetcode-solutions/reverse-string.py
+++ leetcode-solutions/reverse-string.py
@@ -1,17 +1,13 @@
 class Solution:
     def reverseString(self, s: List[str]) -> None:
-        a = 0
-        b = len(s)-1
-        def reverseEnds(s,a,b):
-            if a >= b:
-                return 
-            begin = s[a]
-            end = s[b]
-            s[a] = end
-            s[b] = begin
-            reverseEnds(s,a+1,b-1)
-        reverseEnds(s, 0, len(s) - 1)
-        print(s)
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        i = 0
+        j = len(s)-1
+        while i < j:
+            s[i],s[j] = s[j], s[i]
+            i += 1
+            j -= 1
             
-        
         
