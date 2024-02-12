c37bf70d86cde1505b6a36d775c676d6fd1dcd43
Zelalem Habtamu <108578495+zelalem61@users.noreply.github.com>
2024-02-12 05:43:35 -0800

    Add solution for Middle of the Linked List

--- leetcode-solutions/leetcode/middle-of-the-linked-list.py
+++ leetcode-solutions/leetcode/middle-of-the-linked-list.py
@@ -5,23 +5,16 @@
 #         self.next = next
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        n = 0
-        curr = head
-        while curr:
-            curr = curr.next
-            n +=1
-        middle = (n // 2)+1
-        remo = 1
-        result = ListNode()
-        curre = result
-        while head:
-            if remo >= middle:
-                curre.next = ListNode(int(head.val))
-                curre = curre.next
-            head = head.next
-            remo += 1
-        return result.next
-
+        slow = head
+        fast = head
+        while fast.next:
+            slow = slow.next
+            if fast.next.next != None:
+                fast = fast.next.next
+            else:
+                fast = fast.next
+        head = slow
+        return head
             
             
                 
